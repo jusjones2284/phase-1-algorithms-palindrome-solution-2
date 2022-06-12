cd01317a60193c6bbc35aeaccc8cf23b442454fe
Justin Jones <jus.jones@yahoo.com>
2022-06-12 14:24:51 -0400

    commit tree 557a816afeda17cc1933f61ef49a0f673af6548d

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Write your algorithm here//iterate from the beginning to the middle of the word
+  //racecar
+  //aaaaa 6/3 == 3
+  for(let i = 0; i < word.length/2; i++){
+  
+  const j = word.length - 1 - i
+  const startChar = word[i]
+  const endChar = word[j]
+
+  if (startChar !== endChar) return false;
+
+
 }
+return true;
+}
+
+
+
+function isPalindrome(word){
+
+  for(let i = 0; i < word.length/2; i++){
+
+
+
+    const j = word.length - 1 - i
+    const startChar = word[i]
+    const endChar = word[j]
+
+    if(startChar !== endChar) return false;
+  }
+  return true
+}
+
 
 /* 
   Add your pseudocode here
-*/
+first letter is the same as the last letter, and the second letter is the same as the second to last letter, etc. until we reach the middle, return true. 
 
+iterate from the beginning to the middle check each letter to the corresponding letter from the end if any letters dont match, return false
+
+return true
+
+*/
+ 
 /*
   Add written explanation of your solution here
 */
@@ -13,10 +50,13 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  // console.log("Expecting: true");
+  // console.log("=>", isPalindrome("racecar"));
 
   console.log("");
+  
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abbffba"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
